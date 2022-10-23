7df656cb60c871ca4baa025881e7f2c04a94f616
ilya <zapaska96@gmail.com>
2022-10-23 16:23:26 +0500

    formatter

---
+++ src/utils/digitFormatter.ts
@@ -0,0 +1,10 @@
+const INTEGER_FORMATTER = new Intl.NumberFormat('rus-ru', {
+    maximumFractionDigits: 0,
+});
+
+export function formatOperand(operand: string) {
+    if (operand == '') return;
+    const [integer, decimal] = operand.split('.');
+    if (decimal == null) return INTEGER_FORMATTER.format(+integer);
+    return `${INTEGER_FORMATTER.format(+integer)}.${decimal}`;
+}
